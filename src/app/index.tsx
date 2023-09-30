52b235ca89c54ec6093c58f279cde5be3a975ee4
Nitish Reuben <nitishreuben2600@gmail.com>
2023-09-30 12:21:40 -0700

    changed basename

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -29,7 +29,7 @@ import Layout from '../components/Layout';
  * Making base name for the website (needed in deployment)
  */
 const router = createBrowserRouter(routes, {
-  basename: '/personal-website',
+  basename: '/personal-website/',
 });
 
 function App() {
