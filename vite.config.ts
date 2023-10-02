65e88f4e732c71d3453f715c8265e2cd642c5092
Nitish Reuben <nitishreuben2600@gmail.com>
2023-10-01 18:02:00 -0700

    changed favicon

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/personal-website/assets/',
+  base: '/personal-website/',
   plugins: [react()],
 });
